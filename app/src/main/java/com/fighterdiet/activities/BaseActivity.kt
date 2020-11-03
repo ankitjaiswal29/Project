c84fb6a4d6ffadf43e4d360f82dcac229803e3e6
Atul Raj <contact.atulraj@gmail.com>
2020-11-03 19:00:58 +0530

    Initial Commit

---
+++ app/src/main/java/com/fighterdiet/activities/BaseActivity.kt
@@ -0,0 +1,7 @@
+package com.fighterdiet.activities
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/fighterdiet/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.fighterdiet.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fighterdiet.R
+
+class LoginActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
