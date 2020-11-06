84a004bb3d6410369a73a7c18e3e54da280e8788
Gajendra kumar <gajendra.kumar@quytech.com>
2020-11-06 19:02:21 +0530

    MemberShipActivity Ui Design and ProgressDialog added

--- app/src/main/java/com/fighterdiet/utils/Utils.kt
+++ app/src/main/java/com/fighterdiet/utils/Utils.kt
@@ -1,10 +1,24 @@
 package com.fighterdiet.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 object Utils {
     fun showSnackBar(view: View, message: String) {
         Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun isOnline(context: Context): Boolean {
+        return try {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val info = connectivityManager.activeNetworkInfo
+            info != null && info.isConnected
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
     }
 }
