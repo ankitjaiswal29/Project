2599fc6100ddaa8f03276bf5574fc1666d870ffd
Gajendra kumar <gajendra.kumar@quytech.com>
2020-11-10 23:41:58 +0530

    Change Dashboard Tab icon

--- app/src/main/java/com/fighterdiet/activities/DashboardWithCalaoriesActivity.kt
+++ app/src/main/java/com/fighterdiet/activities/DashboardWithCalaoriesActivity.kt
@@ -11,10 +11,20 @@ import com.fighterdiet.fragments.HomeFragment
 import com.fighterdiet.fragments.HotFragment
 import com.google.android.material.tabs.TabLayout
 
+
 class DashboardWithCalaoriesActivity : BaseActivity() {
     private lateinit var binding: ActivityDashboardWithCalaoriesBinding
     private lateinit var adapter: ViewPagerWithCalDashboardAdapter
     private val fragments = ArrayList<Fragment>()
+    private val tabIcons = intArrayOf(
+        R.mipmap.icn_search,
+        R.mipmap.icn_trending,
+        R.mipmap.icn_favourite,
+        R.mipmap.icn_shopping,
+        R.mipmap.icn_cb,
+        R.mipmap.icn_settings,
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,16 +49,25 @@ class DashboardWithCalaoriesActivity : BaseActivity() {
         fragments.add(cb)
         fragments.add(setting)
 
-        binding.tabs.addTab( binding.tabs.newTab().setText("Self"))
-        binding.tabs.addTab( binding.tabs.newTab().setText("Self"))
-        binding.tabs.addTab( binding.tabs.newTab().setText("Self Me"))
-        binding.tabs.addTab( binding.tabs.newTab().setText("Self Me"))
-        binding.tabs.addTab( binding.tabs.newTab().setText("Self Me"))
-        binding.tabs.addTab( binding.tabs.newTab().setText("Self Me"))
+        binding.tabs.addTab(binding.tabs.newTab().setText(""))
+        binding.tabs.addTab(binding.tabs.newTab().setText(""))
+        binding.tabs.addTab(binding.tabs.newTab().setText(""))
+        binding.tabs.addTab(binding.tabs.newTab().setText(""))
+        binding.tabs.addTab(binding.tabs.newTab().setText(""))
+        binding.tabs.addTab(binding.tabs.newTab().setText(""))
         binding.tabs.tabGravity = TabLayout.GRAVITY_FILL
 
         setupTabs()
+        setupTabIcons()
+    }
 
+    private fun setupTabIcons() {
+        binding.tabs.getTabAt(0)?.setIcon(tabIcons .get(0))
+        binding.tabs.getTabAt(1)?.setIcon(tabIcons .get(1))
+        binding.tabs.getTabAt(2)?.setIcon(tabIcons .get(2))
+        binding.tabs.getTabAt(3)?.setIcon(tabIcons .get(3))
+        binding.tabs.getTabAt(4)?.setIcon(tabIcons .get(4))
+        binding.tabs.getTabAt(5)?.setIcon(tabIcons .get(5))
     }
 
     private fun setupTabs() {
@@ -80,9 +99,5 @@ class DashboardWithCalaoriesActivity : BaseActivity() {
 
             }
         })
-    }
-
-    private fun setupIndicator(position: Int) {
-
     }
 }
