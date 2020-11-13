8dc46d27c31c3d39045f20902f3cc7547f87f703
Gajendra kumar <gajendra.kumar@quytech.com>
2020-11-13 11:50:11 +0530

    pic day seven and one day ui modify

--- app/src/main/java/com/fighterdiet/activities/PicDaySevenMealsActivity.kt
+++ app/src/main/java/com/fighterdiet/activities/PicDaySevenMealsActivity.kt
@@ -4,12 +4,44 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fighterdiet.R
+import com.fighterdiet.adapters.HomeFragmentRecyclerAdapter
+import com.fighterdiet.databinding.ActivityPicDaySevenMealsBinding
+import com.fighterdiet.models.home_frag.HomeModel
+import com.fighterdiet.utils.Utils
 
 class PicDaySevenMealsActivity : AppCompatActivity() {
+    lateinit var binding:ActivityPicDaySevenMealsBinding
+    private lateinit var homeAdapter : HomeFragmentRecyclerAdapter
+    var homeList:ArrayList<HomeModel> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pic_day_seven_meals)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_pic_day_seven_meals)
+        initialise()
+    }
+
+    private fun initialise() {
+        setUpHomeList()
+        setUpHomeRecyclerView()
+    }
+
+    private fun setUpHomeList() {
+        homeList.add(HomeModel(R.mipmap.food_1))
+        homeList.add(HomeModel(R.mipmap.food_2))
+        homeList.add(HomeModel(R.mipmap.food_3))
+    }
+
+    private fun setUpHomeRecyclerView() {
+
+        binding.rvPicSeven.layoutManager = LinearLayoutManager(this)
+        homeAdapter = HomeFragmentRecyclerAdapter(this,homeList){
+                position,view ->
+            Utils.showSnackBar(binding.rvPicSeven,"mes")
+        }
+        binding.rvPicSeven.adapter = homeAdapter
     }
 
     companion object {
--- app/src/main/java/com/fighterdiet/activities/PicOneDayMealsActivity.kt
+++ app/src/main/java/com/fighterdiet/activities/PicOneDayMealsActivity.kt
@@ -4,12 +4,44 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fighterdiet.R
+import com.fighterdiet.adapters.HomeFragmentRecyclerAdapter
+import com.fighterdiet.databinding.ActivityPicOneDayMealsBinding
+import com.fighterdiet.models.home_frag.HomeModel
+import com.fighterdiet.utils.Utils
 
-class PicOneDayMealsActivity : AppCompatActivity() {
+class PicOneDayMealsActivity : BaseActivity() {
+    lateinit var binding:ActivityPicOneDayMealsBinding
+    private lateinit var homeAdapter : HomeFragmentRecyclerAdapter
+    var homeList:ArrayList<HomeModel> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pic_one_day_meals)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_pic_one_day_meals)
+        initialise()
+    }
+
+    private fun initialise() {
+        setUpHomeList()
+        setUpHomeRecyclerView()
+    }
+
+    private fun setUpHomeList() {
+        homeList.add(HomeModel(R.mipmap.food_1))
+        homeList.add(HomeModel(R.mipmap.food_2))
+        homeList.add(HomeModel(R.mipmap.food_3))
+    }
+
+    private fun setUpHomeRecyclerView() {
+
+        binding.rvOneDay.layoutManager = LinearLayoutManager(this)
+        homeAdapter = HomeFragmentRecyclerAdapter(this,homeList){
+                position,view ->
+            Utils.showSnackBar(binding.rvOneDay,"mes")
+        }
+        binding.rvOneDay.adapter = homeAdapter
     }
 
     companion object {
