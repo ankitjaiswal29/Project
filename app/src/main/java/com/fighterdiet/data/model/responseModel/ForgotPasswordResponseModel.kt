fd2e7f78a2415f1a28ccdaf5eabceca76b096dae
ankit <kumarankit.jaiswal29@gmail.com>
2021-10-04 13:30:54 -0400

    api

--- app/src/main/java/com/fighterdiet/data/model/responseModel/ForgotPasswordResponseModel.kt
+++ app/src/main/java/com/fighterdiet/data/model/responseModel/ForgotPasswordResponseModel.kt
@@ -2,13 +2,12 @@ package com.fighterdiet.data.model.responseModel
 
 data class ForgotPasswordResponseModel(
     val code: Int,
-    val data: ForgotPasswordData,
+    val `data`: ForgotPasswordData,
     val message: String,
     val status: Boolean
-){
-    data class ForgotPasswordData(
-        val email: String,
-        val otp: Int,
-        val user_id: Int
-    )
-}
+)
+data class ForgotPasswordData(
+    val email: String,
+    val otp: Int,
+    val user_id: Int
+)
